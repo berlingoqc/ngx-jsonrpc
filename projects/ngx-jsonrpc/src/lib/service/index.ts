cd83d5dffdc7757174356e6fb1edc663efadca31
William Quintal <william95quintalwilliam@outlook.com>
2019-10-21 17:16:18 -0400

    lololol

--- projects/ngx-jsonrpc/src/lib/service/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './dec';
-export * from './jsonrpc-client';
-export * from './jsonrpc-client-ws';
-export * from './jsonrpc-settings';
-export * from './rpc-call';
-export * from './utility-reflection';
--- projects/ngx-jsonrpc/src/public-api.ts
+++ projects/ngx-jsonrpc/src/public-api.ts
@@ -2,5 +2,9 @@
  * Public API Surface of ngx-jsonrpc
  */
 
-export * from './lib/service';
+export * from './lib/service/jsonrpc-client-ws';
+export * from './lib/service/jsonrpc-client';
+export * from './lib/service/jsonrpc-settings';
+export * from './lib/service/rpc-call';
+export * from './lib/service/impl/rpc-system-call';
 export * from './lib/ngx-jsonrpc.module';
